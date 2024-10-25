767a9149919c33ee2d51850a93b89019fda3b8a5
AmanCH3 <armanachaudhary53@gmail.com>
2024-10-25 00:50:18 -0700

    feat : menu models

--- menus/models.py
+++ menus/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    category = models.CharField(max_length = 255)
+    item_name = models.CharField(max_length = 255)
+    price = models.FloatField()
+    admin = models.ForeignKey('staff.Administrator', on_delete = models.SET_NULL, null=True)
+
+
+class MenuItem(models.Model):
+    menu = models.ForeignKey(Menu, on_delete = models.CASCADE)
+    item_name = models.CharField(max_length = 255)
+    price = models.FloatField()
+
+
+
+class Recipe(models.Model):
+    chef_name = models.CharField(max_length = 255)
+    preparation_instrution = models.TextField()
+
+
+class Ingredient(models.Model):
+    ingredient_name = models.CharField(max_length = 255)
+    quality =models.CharField(max_length = 255)
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete = models.CASCADE)
+  
