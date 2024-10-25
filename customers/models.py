423b6a6fe9c552b913139bff66ce1d63b54beffc
AmanCH3 <armanachaudhary53@gmail.com>
2024-10-25 00:44:34 -0700

    feat : Customer Models

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length = 255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=12)
+    address = models.TextField()
+    memebership_status = models.CharField(max_length = 50)
+   
