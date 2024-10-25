26c094265d854d00c715f7bdf049481a518604db
AmanCH3 <armanachaudhary53@gmail.com>
2024-10-25 00:46:27 -0700

    Feat : Reservation table

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Reservation(models.Model):
+    customer = models.ForeignKey('customers.Customer', on_delete = models.CASCADE)
+    table = models.ForeignKey('tables.Table', on_delete = models.CASCADE)
+    admin = models.ForeignKey('staff.Administrator', on_delete = models.SET_NULL, null=True)
+    reservation_date = models.DateTimeField(auto_now_add = True)
+    reservation_time = models.TimeField()
