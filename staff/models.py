991e8d09e6a010f8874986bc4b41ec25d7858fbc
AmanCH3 <armanachaudhary53@gmail.com>
2024-10-25 00:53:20 -0700

    Feat : staf models

--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Waiter(models.Model):
+    name = models.CharField(max_length = 255)
+    schedule = models.CharField(max_length = 255)
+
+
+class Table(models.Model):
+    capacity = models.IntegerField()
+    availability_status = models.CharField(max_length = 50)
+
+
+
+class Administrator(models.Model):
+    name = models.CharField(max_length = 255)
+    username = models.CharField(max_length = 255)
+    password = models.CharField(max_length = 255)
