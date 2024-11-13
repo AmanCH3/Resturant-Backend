c681f18445a606bd12e818511c3da867b40171bd
AmanCH3 <armanachaudhary53@gmail.com>
2024-11-13 12:13:11 +0545

    feat : Manage and update orders by staff

--- staff/urls.py
+++ staff/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('orders/', views.OrderListCreate.as_view(), name='orders-list-create'),
-    
+    path('orders/manage/', views.manage_orders, name='manage_orders'),
+    path('orders/update/<int:order_id>/', views.update_order_status, name='update_order_status'),
 ]
--- staff/views.py
+++ staff/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
+from orders.models import Order
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
+@staff_member_required
+def manage_orders(request):
+    orders = Order.objects.all().order_by("-order_date")
+    if request.method == "POST":
+        order_id = request.POST.get("order_id")
+        new_status = request.POST.get("status")
+        order = Order.objects.get(pk=order_id)
+        order.status = new_status
+        order.save()
+        return JsonResponse({"message": "Order status updated successfully!"})
+    
+    context = {
+        "orders" : orders ,
+        "status_choices": Order.STATUS_CHOICES
+    }
+    return JsonResponse({"message": "List of orders", "data": context})
+
+
+# Update order status
+@staff_member_required
+@require_POST
+def update_order_status(request, order_id):
+    order = get_object_or_404(Order, pk=order_id)
+    status = request.POST.get("status")
+    if status in dict(Order.STATUS_CHOICES):
+        order.status = status
+        order.save()
+        return JsonResponse({"message": "Order status updated successfully."})
+    return JsonResponse({"error": "Invalid status."}, status=400)
