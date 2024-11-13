2174f5367361c5a511adaf66fadfabed5ec139cf
AmanCH3 <armanachaudhary53@gmail.com>
2024-11-13 11:49:49 +0545

    fix : model fixed

--- orders/models.py
+++ orders/models.py
@@ -2,17 +2,21 @@ from django.db import models
 
 # Create your models here.
 
-class Order(models.Model) :
-    order_id = models.AutoField(primary_key = True)
-    customer = models.CharField('customers.customer', on_delete = models.CASCADE)
-    order_status = models.CharField(max_length = 50)
-    total_price = models.DecimalField(max_digits = 10, decimal_places = 2)
-    order_date = models.DateTimeField(auto_now_add = True)
-    admin = models.ForeignKey('staff.Administarator', on_delete = models.SET_NULL , null=True)
-
-
-
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey('customers.Customer', on_delete=models.CASCADE)
+    
+    STATUS_CHOICES = [
+        ('paid', 'Paid'),
+        ('Packed', 'packed'),
+        ('Recieved', 'recieved'),
+    ]
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="In Progress")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    order_date = models.DateTimeField(auto_now_add=True)
+    admin = models.ForeignKey('staff.Administrator', on_delete=models.SET_NULL, null=True)
+    
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order , on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     menu_item = models.ForeignKey("menus.MenuItem", on_delete=models.CASCADE)
     quantity = models.IntegerField()
